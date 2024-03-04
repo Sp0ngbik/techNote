3d7b8ec8bfa3b24f49ac2f1de8e52b0757837e00
telonasport99 <telonasport99@gmail.com>
2024-03-05 00:10:20 +0300

    header

---
+++ src/app/advertising/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            advertising
+        </div>
+    );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import styles from "./page.module.css";
-import './styles/index.scss'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
         Hello World
     </main>
   );
