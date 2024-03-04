72fa8fb1623b610cd8463f769f64b69a5affeb58
Vlad <Sp0ngbik@gmail.com>
2024-03-04 20:03:24 +0300

    feature/ui-boiler-styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import styles from "./page.module.css";
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 export default function Home() {
   return (
