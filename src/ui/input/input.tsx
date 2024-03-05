f5e09555e2c21f687d37e29eeda940be3bd33540
chepeLove <ilya.chepelov@gmail.com>
2024-03-05 13:04:02 +0300

    fix: change import Input

--- src/ui/input/input.tsx
+++ src/ui/input/input.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import s from './input.module.scss'
 
-const Input = () => {
+export const Input = () => {
     return (
       <input placeholder={'Поиск по блогу'} className={s.input}/>
     );
 };
 
-export default Input;
