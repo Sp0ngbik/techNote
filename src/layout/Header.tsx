ac70a67daf23af0b3b80247eaf364242a1e1f6ec
Sergey <sergbfmv@mail.ru>
2024-03-05 15:33:58 +0300

    feat:  add card component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Link from "next/link";
 import s from './Header.module.scss'
-import { Input } from '@/ui/input/input';
+import {Input} from '@/ui/input/input';
+
 const Header = () => {
     return (
         <header className={s.header}>
-          <div className={s.links} >
-              <Link className={s.link} href={'/'}>Главная</Link>
-              <Link className={s.link} href={'/about'}>Обо мне</Link>
-              <Link className={s.link} href={'/advertising'}>Реклама</Link>
-          </div>
+            <div className={s.links}>
+                <Link className={s.link} href={'/'}>Главная</Link>
+                <Link className={s.link} href={'/about'}>Обо мне</Link>
+                <Link className={s.link} href={'/advertising'}>Реклама</Link>
+            </div>
             <Input/>
         </header>
     );
